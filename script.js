a8c4a064383141e416ad88c23ee8f415fdff6262
LB123658 <79328251+LB123658@users.noreply.github.com>
2023-09-05 00:19:32 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", () => {
+  registerSW();
+});
+async function registerSW() {
+  if ("serviceWorker" in navigator) {
+    try {
+      await navigator.serviceWorker.register("serviceworker.js");
+    } catch (e) {
+      console.log("SW registration failed");
+    }
+  }
+}
+var beforeInstallPrompt = null;
+window.addEventListener("beforeinstallprompt", eventHandler, errorHandler);
+function eventHandler(event) {
+  beforeInstallPrompt = event;
+}
+function errorHandler(event) {
+  console.log("error: " + event);
+}
+function install() {
+  if (beforeInstallPrompt) beforeInstallPrompt.prompt();
+}
